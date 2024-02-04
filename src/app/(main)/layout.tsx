15775828452dd2fb2077b546493764d2e88fc9c8
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2024-02-03 20:03:49 -0500

    fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -17,17 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      {children}
-    </div>
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TRPCReactProvider>
             <div className="flex h-screen flex-col">
               <Header />
-              <div className="h-full overflow-hidden px-8 py-4">{children}</div>
+              <div className="h-full overflow-hidden">{children}</div>
             </div>
           </TRPCReactProvider>
         </ThemeProvider>
